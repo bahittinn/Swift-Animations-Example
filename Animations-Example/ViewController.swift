6f4e87b9176a7d0fa3a976eab1a12fa7434b6715
bahittinn <bahittin84@gmail.com>
2023-09-16 22:15:08 +0300

    animation buttons

--- Animations-Example/ViewController.swift
+++ Animations-Example/ViewController.swift
@@ -13,9 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var exampleLabel: UILabel!
     
+    @IBOutlet weak var animationButton: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    
+    var animationState:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animationButton.layer.cornerRadius = animationButton.frame.size.width / 2
+        button1.layer.cornerRadius = button1.frame.size.width / 2
+        button2.layer.cornerRadius = button2.frame.size.width / 2
+        
+        button1.alpha = 0
+        button2.alpha = 0
+        self.button1.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        self.button2.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
     }
     
     @IBAction func startButtoClicked(_ sender: UIButton) {
@@ -37,5 +51,29 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func animationButtonClicked(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0) {
+            if self.animationState {
+                self.button1.alpha = 0
+                self.button2.alpha = 0
+                
+                self.animationButton.transform = CGAffineTransform(rotationAngle: 0 * .pi/180)
+                self.button1.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                self.button2.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                
+                self.animationState = false
+                
+            } else {
+                self.button1.alpha = 1
+                self.button2.alpha = 1
+                
+                self.animationButton.transform = CGAffineTransform(rotationAngle: 45 * .pi/180)
+                self.button1.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.button2.transform = CGAffineTransform(scaleX: 1, y: 1)
+                
+                self.animationState = true
+            }
+        }
+    }
 }
 
