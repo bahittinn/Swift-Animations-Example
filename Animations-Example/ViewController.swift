04d29a4dba5d9935f66cc7def0d6ee0695451e4d
bahittinn <bahittin84@gmail.com>
2023-09-16 21:26:29 +0300

    alpha animation

--- Animations-Example/ViewController.swift
+++ Animations-Example/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var exampleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func startButtoClicked(_ sender: UIButton) {
+        alphaAnimation()
+    }
+    
+    func alphaAnimation() {
+        self.imageView.alpha = 0
+        
+        UIView.animate(withDuration: 3, delay: 0) {
+            self.imageView.alpha = 1
+        }
+    }
 }
 
